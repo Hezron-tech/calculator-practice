2cabe7c4885065727742283d1bf9b48c03d38ac5
Hezron-tech <kiprophez@gmail.com>
2022-01-26 13:34:18 +0300

    adding js functions

---
+++ script.js
@@ -0,0 +1,24 @@
+let result=document.getElementById("inputtext")
+
+let calculate =(number) =>{
+    result.value=result.value+ number;
+}
+
+let Results =() =>{
+    try
+    {
+       
+        result.value=eval(result.value)
+
+    }
+    catch(err){
+alert("enter valid values");
+    }
+}
+
+function clr(){
+    result.value =" ";
+}
+function del(){
+    result.value=result.value.slice(0,-1);
+}
